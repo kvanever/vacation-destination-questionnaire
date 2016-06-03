03bcd0208654f9a55fe904d27605e4c6dce083c8
Kevin VanEvery <kvanever@gmail.com>
2016-06-03 09:28:31 -0700

    declare variable for each vacation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 /*This file is for your custom js.  All yours*/
+
+// Declare global variable for each vacation
+
+var washingtonDC;
+var newOrleans;
+var ecuador;
+var newZealand;
+var newEngland;
+var Alaska;
+var Greece;
+var Japan;
+var India;
+var England;
